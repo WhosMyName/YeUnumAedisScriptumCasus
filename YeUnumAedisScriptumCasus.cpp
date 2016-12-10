43c15c341673ba0e48c01b101399877cdd062afb
WhosMyName <WhosMyName@users.noreply.github.com>
2016-12-10 20:49:23 +0100

    finial?

--- YeUnumAedisScriptumCasus.cpp
+++ YeUnumAedisScriptumCasus.cpp
@@ -20,8 +20,10 @@ bool isWhite = false;
 bool thread_running = false;
 bool isLight = false;
 bool portal = false;
+bool has_scroll = true;
 int lvl = 0;
 int delim;
+int candleStage = 0;
 std::string fontfile = "hachicro.ttf" ;
 std::vector<std::string> history;
 std::vector<std::string>::iterator it = history.begin();
@@ -63,6 +65,25 @@ void flush(){
 	outputstream.str(std::string());
 }
 
+void help(){
+    if(lvl == 0){
+        flush();
+        print("You find yourself surrounded by darkness.\nYou feel a strong wind and tight shoes around your feet.\nThere is a CANDLE and a SCROLL in front of you.");
+    }
+    else if(lvl == 1){
+        flush();
+        print("You find yourself surrounded by darkness.\nYou feel a mild breeze.\nThere is a CANDLE and a SCROLL in front of you.");
+    }
+    else if(lvl == 2){
+        flush();
+        print("You find yourself surrounded by darkness.\nYou feel no movement of the air.\nThere is a CANDLE and a SCROLL in front of you.");
+    }
+    else if(lvl == 3){
+        flush();
+        print("THANK YOU FOR PLAYING\nYE UNUM AEDIS SCRIPTUM CASUS!");
+    }
+}
+
 void read(std::string obj){
     if(isLight){
         if(obj == "scroll" ){
@@ -76,10 +97,7 @@ void read(std::string obj){
         }
         else if(obj == "spell"){
             flush();
-            print("As you read out the magic spell, a PORTAL made out of energy opens.");
-            lvl++;
-            isLight = false;
-            portal = false;
+            print("As you read out the magic spell,\na PORTAL made out of energy opens.");
         }
         else{
             flush();
@@ -94,13 +112,42 @@ void read(std::string obj){
     }
 }
 void light(std::string obj){
-
+    if(obj == "scroll"){
+        flush();
+        bool has_scroll = true;
+        print("As the Scroll blazed into dust,\nyou realize that you just burned your only chance of Escape!\n gg wp");
+    }
+    else if(obj == "candle"){
+        flush();
+        isLight = true;
+        print("A faint light fills the room!");
+    }
+    else if(obj == "self"){
+       print("As your Entity is embraces by the warmth of the Flames,\nyou realize that you left Stove turned on at home!");
+       isLight = true;
+    }
 }
 void roll(std::string obj){
-
+    if(obj == "portal" && portal){
+        lvl++;
+        isLight = false;
+        portal = false;
+        flush();
+        print("As you roll through the Portal,\nyour Vision blurs\n and a slight breeze extinguishes the Flame of your Candle.");
+    }
+    else if(obj == "portal" && !portal){
+        flush();
+        print("There's no Portal to roll into!\nWhat were you trying to achieve?");
+    }
+    else{
+        flush();
+        print("You roll around aimlessly.");
+        std::this_thread::sleep_for(std::chrono::seconds(10));
+        flush();
+        print("You are tired of rolling around.\nMaybe you should focus on your tasks.");
+    }
 }
 
-int candleStage = 0;
 void play(std::string obj){
 	if(obj == "candle" && isLight){
 		candleStage = 1;
@@ -144,8 +191,12 @@ void walk(std::string obj){
 		print("You trip. You fall.\n\n You realize that you are wearing\n roller blades.\n You cannot walk. You can only ROLL.");
 	}
 }
-void fall(std::string obj){
-
+void fall(){
+    flush();
+    print("You do a 1080° Full-Frontal Node-Dive.");
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    flush();
+    print("This actually hurts!");
 }
 
 void switch_context(std::string in){ //Main Function to Play
@@ -171,10 +222,13 @@ void switch_context(std::string in){ //Main Function to Play
         play(obj);
     }
     else if(command == "fall"){
-        fall(obj);
+        fall();
+    }
+    else if(command == "help"){
+        help();
     }
     else{
-        std::cout << "This is not a valid Option!" << std::endl;
+        std::cout << "This is not a valid Option!\nNeed some help?" << std::endl;
     }
 }
 
@@ -183,7 +237,7 @@ int main(){
     //Use Booleans to mark a pressed key for smooth Movement-Systems or real-time Keyboard-Event
     //
     //-----------------------------------------------------------------------------------------------------*/
-    int w = 1280;
+    int w = 1650;
     int h = 720;
     window.create(sf::VideoMode(w, h, 32), "Enter Title here!"); //Creates Window
     window.setFramerateLimit(60);
@@ -206,10 +260,10 @@ int main(){
         entry. setPosition(0, (h - 32));
         text.setPosition(entry.getCharacterSize(), (h - 32)); //Initial Text Position
         cursor.setPosition(text.getGlobalBounds().width, (h - 32));//Initial Cursor Position
-	output.setFont(font);
-        output.setCharacterSize(24);
+	    output.setFont(font);
+        output.setCharacterSize(18);
         output.setColor(sf::Color::White);
-        output.setPosition(w/3, (h/3));
+        output.setPosition(0, (h/3));
     }
     thread_running = true;
     std::thread thread(blinkCursor);// Creates Thread (C++11) and calls blinkCursor
@@ -218,7 +272,7 @@ int main(){
         sf::Vector2f pos = text.getPosition();
         sf::Vector2f pos2 = cursor.getPosition();
         int cmd_size = (5 + entry.getGlobalBounds().width + text.getLocalBounds().width);
-        
+        help();
         while (window.pollEvent(event)){
             if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)){
                 thread_running = false;
